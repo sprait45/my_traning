6523ff49a72a02baedf83bb1aff15c661f50ec5e
sprait45 <119442580+sprait45@users.noreply.github.com>
2024-11-03 18:58:57 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,10 @@
+from fake_math import divide as fun1
+from true_math import divide as fun2
+result1 = fun1(27,3)
+result2 = fun1(7,0)
+result3 = fun2(9,3)
+result4 = fun2(65,0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
